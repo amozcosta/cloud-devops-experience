f1494ea3a1e97e6cbc5972a55b42135014ab61a7
amozcosta <amozcosta@gmail.com>
2022-11-11 12:01:19 -0300

    script para provisionar apache2

--- provisioning-apache2.sh
+++ provisioning-apache2.sh
@@ -6,12 +6,11 @@
 # copiar os arquivos para o /var/www/html
 # subir script para o github
 
-apt update 
+echo "Atualizando e instalando softwares necessários"
+apt update && apt upgrade -y && apt install apache2 curl unzip
 
-apt install apache2 curl unzip
-
+echo "Baixando e copiando arquviso da aplicação"
 cd /tmp 
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 
 unzip main.zip -d /var/www/html
-
